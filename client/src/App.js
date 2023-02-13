00fef61865c9b002d53d6346c6ecce526c2d2b67
venkat0086 <venkygowdru99@gmail.com>
2023-02-13 18:32:00 +0530

    Added and commited New Spotify Project

---
+++ client/src/App.js
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { accessToken, logout } from "./spotify";
+import { GlobalStyle } from "./styles";
+import styled from "styled-components/macro";
+import {
+  Login,
+  Profile,
+  TopArtists,
+  TopTracks,
+  Playlists,
+  Playlist,
+} from "./pages";
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0, 0, 0, 0.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
+
+// Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    setToken(accessToken);
+  }, []);
+
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <header className="App-header">
+        {!token ? (
+          <Login />
+        ) : (
+          <>
+            <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/top-artists" element={<TopArtists />} />
+              <Route path="/top-tracks" element={<TopTracks />} />
+              <Route path="/playlists/:id" element={<Playlist />} />
+              <Route path="/playlists" element={<Playlists />} />
+              <Route path="/" element={<Profile />} />
+            </Routes>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/styles/StyledDropdown.js
@@ -0,0 +1,37 @@
+import styled from "styled-components/macro";
+
+const StyledDropdown = styled.div`
+  position: absolute;
+  top: 0;
+  right: var(--spacing-md);
+  z-index: 1;
+
+  @media (min-width: 768px) {
+    right: var(--spacing-xxl);
+  }
+
+  &:after {
+    content: "";
+    top: 15px;
+    right: var(--spacing-sm);
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-top: 6px solid var(--black);
+  }
+
+  select {
+    -webkit-appearance: none;
+    appearance: none;
+    border: 0;
+    border-radius: var(--border-radius-subtle);
+    font-size: var(--fz-sm);
+    font-family: inherit;
+    padding: var(--spacing-xs) var(--spacing-xl) var(--spacing-xs)
+      var(--spacing-sm);
+  }
+`;
+
+export default StyledDropdown;
